ab2bbe7929935ff8886071b50015ef4b3eddd916
marcosajm <52014409+marcosajm@users.noreply.github.com>
2022-11-27 06:50:27 +0000

    Update Automoveis.java

--- src/Automoveis.java
+++ src/Automoveis.java
@@ -1,95 +1,28 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        List<Automoveis> automoveisList = new ArrayList<Automoveis>();
-        // ArrayList<String> listaCorres = new ArrayList<>();
-        Scanner scan1 = new Scanner(System.in);
-        Collections.sort(automoveisList);
-        while (true) {
-            Automoveis p = new Automoveis();
-            boolean ans = automoveisList.isEmpty();
-            int size = automoveisList.size();
-            System.out.println("Lista de automóveis");
-            System.out.println("Escolha uma opção (1 - 4):");
-            System.out.println("1 - Mostra a lista. \n2 - Inserir novo automóvel. \n3 - Apagar automóvel. \n4 - Sair." + "\n ");
-            int task = scan1.nextInt();
-            if (task == 4) {
-                break;
-            }
-            switch (task) {
-                case 1:
-                    if (ans) {
-                        System.out.println("Lista vazia! " + "\n ");
-                        break;
-                    }
-                    var first = automoveisList.get(0);
-                    var last = automoveisList.get(size - 1);
-                    var lastP = automoveisList.get(p.getIndex());
-                    System.out.println( lastP + "List is " + automoveisList + "\n ");
-                    System.out.println("Last automóvel : " + last + "\n " + "first automóvel : " + first + "\n ");
-                    System.out.println("Quantos automóveis existem na lista: " + size + "\n ");
-                    System.out.println("Nome do automóvel no início da lista é: " + automoveisList.get(0).getName() + "\n marca: " + automoveisList.get(0).getMarca() + "\n número index: " + automoveisList.get(0).getIndex() +"\n" );
-                    break;
-                case 2:
-                    do {
-                        System.out.println("Entre com o número index do automóvel: ");
-                        p.setIndex(scan1.nextInt());
-                        System.out.println("Entre com o nome: ");
-                        p.setName(scan1.next());
-                        System.out.println("Inserir a marca:");
-                        p.setMarca(scan1.next());
-                        automoveisList.add(p);
-                        System.out.println("Deseja adicionar mais automóveis? (Y/N)");
-                        if (scan1.next().equalsIgnoreCase("N")) {
-                            break;
-                        }
-                        // listaCorres.add(scan1.next());
-                    }
-                    while (true);
-                    break;
-                case 3:
-                    if (ans) {
-                        System.out.println("Lista vazia! " + "\n ");
-                        break;
-                    }
-                    do {
-                        Scanner sc = new Scanner(System.in);
-                        System.out.println("Entre com o nome do automóvel: " + "\n ");
-                            // p.setMarca(sc.next());
-                            automoveisList.remove(sc.next());
-                            //  automoveisList.remove(Object o);
-                            //  automoveisList.removeAll(Collections.singleton("A"));   //[B, D]
-                            // automoveisList.remove(0);
-                        System.out.println("Deseja apagar mais automóveis? (Y/N)");
-                        if (sc.next().equalsIgnoreCase("N")) {
-                            break;
-                        }
-                    }
-                    while (true);
-                    break;
-                default:
-                    System.out.println("Invalido!" + "\n ");
-                    break;
-            }
-        }
+class Automoveis implements Comparable{
+    int index;
+    String name;
+    String marca;
+    public int getIndex() {
+        return index;
+    }
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    @Override
+    public int compareTo(Object arg0) {
+        Automoveis p = (Automoveis)arg0;
+        return p.getIndex();
     }
 }
-// Collections.sort(automoveisList);
-     //   System.out.println("Nome do automóveis no início da lista é: " + automoveisList.get(0).getName() + "\n número de telefone: " + automoveisList.get(0).getMarca());
-       // Java Collections
-        //  Exercício 1: Crie uma ArrayList de Strings que permite guardar
-        //   marcas de automóveis. Crie funções que permitem:
-        //  • Adicionar um carro - nomeDaArrayList.add(“nome”);
-        //  • Adicionar um carro e indicar o índice – nomeDaArrayList.add(1,“nome”);
-        //  • Remover elemento pelo seu nome –
-        //  nomeDaArrayList.remove(“nome”);
-        //  • Remover elemento pelo seu índice –
-        //  nomeDaArrayList.remove(1);
-
-     //   Collections.reverse(list);
-     //   list.add(elementForTop);
-    //    Collections.reverse(list);
